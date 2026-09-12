079096f8b9a9c6667c6345e52595c0126049ee6e
Da7em-T <Da7.world@protonmail.com>
2026-09-12 22:11:41 +0300

    Cover string-set, string-swap!, string-shrink!, and substring-fill! in walker-visible tests.

--- tests/unit-tests/05-char/string_mutate.scm
+++ tests/unit-tests/05-char/string_mutate.scm
@@ -10,6 +10,23 @@
   (let ((s (string #\a #\b #\c #\d #\e)))
     (string-fill! s #\x)
     s))
+(test-equal "abxde" (string-set "abcde" 2 #\x))
+(test-equal "abdce"
+  (let ((s (string #\a #\b #\c #\d #\e)))
+    (string-swap! s 2 3)
+    s))
+(test-equal "abc"
+  (let ((s (string #\a #\b #\c #\d #\e)))
+    (string-shrink! s 3)
+    s))
+(test-equal "axxde"
+  (let ((s (string #\a #\b #\c #\d #\e)))
+    (substring-fill! s 1 3 #\x)
+    s))
 (test-error-tail type-exception? (string-copy 1))
 (test-error-tail type-exception? (string-set! 1 0 #\x))
 (test-error-tail type-exception? (string-fill! 1 #\x))
+(test-error-tail type-exception? (string-set 1 0 #\x))
+(test-error-tail type-exception? (string-swap! 1 0 1))
+(test-error-tail type-exception? (string-shrink! 1 0))
+(test-error-tail type-exception? (substring-fill! 1 0 1 #\x))
